13f372dcd4fc5a7c8d8b5f4d88145f138670b923
Moreen <moreenkim36@gmail.com>
2020-09-23 12:18:23 +0300

    Add mongoose error handling functionality

--- middleware/error.js
+++ middleware/error.js
@@ -5,7 +5,7 @@ const errorHandler = (err, req, res, next) => {
 
   error.message = err.message;
   //log to console for dev
-  console.log(err.stack.red);
+  console.log(err);
 
   //mongoose bad obj id
   if (err.name === 'CastError') {
@@ -13,6 +13,17 @@ const errorHandler = (err, req, res, next) => {
     error = new ErrorResponse(message, 404);
   }
 
+  //mongoose duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  //mongoose validation error
+  if (err.name === 'ValidationError') {
+  }
+  const message = Object.values(err.errors).map((val) => val.message);
+  error = new ErrorResponse(message, 400);
   res
     .status(error.statusCode || 500)
     .json({ success: false, error: error.message || 'Server Error' });
