4d7b38a07cb8ccc7f29aeff784d1f790841bdbf0
Moreen <moreenkim36@gmail.com>
2020-09-30 12:57:47 +0300

    Add database seeder for retrieving and destroying data

---
+++ seeder.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+//load env variables
+dotenv.config({ path: './config/config.env' });
+
+//load models
+const Bootcamp = require('./models/Bootcamp');
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+//read json files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+//import data from db
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log('Data imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+//delete data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log('Data destroyed...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
