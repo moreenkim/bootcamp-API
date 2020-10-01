ad543376ce0d5089bac4cb2bb9bf1ea68bfa05e5
Moreen <moreenkim36@gmail.com>
2020-10-01 10:08:39 +0300

    Create course routes controller

---
+++ routes/courses.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {
+  getCourses,
+  //   getBootcamp,
+  //   createBootcamp,
+  //   updateBootcamp,
+  //   deleteBootcamp,
+  //   getBootcampsInRadius,
+} = require('../controllers/courses');
+
+const router = express.Router({ mergeParams: true });
+
+router.route('/').get(getCourses);
+
+module.exports = router;
