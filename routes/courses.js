a0242551b7ad8add06f1b7443d5aa15f15364eca
Moreen <moreenkim36@gmail.com>
2020-10-01 11:52:17 +0300

    Update the courses and Delete courses functionalities

--- routes/courses.js
+++ routes/courses.js
@@ -3,14 +3,14 @@ const {
   getCourses,
   getCourse,
   addCourse,
-  //   updateBootcamp,
-  //   deleteBootcamp,
+  updateCourse,
+  deleteCourse,
   //   getBootcampsInRadius,
 } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse);
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
