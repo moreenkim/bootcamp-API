987a86e41f0a8e849075064a7c339a4dadb43b92
Moreen <moreenkim36@gmail.com>
2020-10-02 12:35:47 +0300

    Implement user relationships functionality

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -30,6 +30,22 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 //route    POST api/v1/bootcamps
 //access Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  //add user to req.body
+  req.body.user = req.user.id;
+
+  //check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  //if not admin they can add 1 bootcamp only
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `the user with id ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
+
   const bootcamp = await Bootcamp.create(req.body);
 
   res.status(201).json({
